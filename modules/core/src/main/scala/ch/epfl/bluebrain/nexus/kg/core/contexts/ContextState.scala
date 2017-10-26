0c6db59ce9bceb4b7f37ef4f3fd352d39f761785
Bogdan Roman <bogdan.roman@epfl.ch>
2017-10-26 14:37:52 +0200

    Defined Context specific data types

---
+++ modules/core/src/main/scala/ch/epfl/bluebrain/nexus/kg/core/contexts/ContextState.scala
@@ -0,0 +1,29 @@
+package ch.epfl.bluebrain.nexus.kg.core.contexts
+
+import io.circe.Json
+
+/**
+  * Enumeration type for possible states of a context.
+  */
+sealed trait ContextState extends Product with Serializable
+
+object ContextState {
+
+  /**
+    * Initial state for all contexts.
+    */
+  final case object Initial extends ContextState
+
+  /**
+    * State used for all contexts that have been created and later possibly updated, deprecated or published.
+    *
+    * @param id         the unique identifier of the context
+    * @param rev        the selected revision number
+    * @param value      the json representation of the context
+    * @param published  the publish status
+    * @param deprecated the deprecation status
+    */
+  final case class Current(id: ContextId, rev: Long, value: Json, published: Boolean, deprecated: Boolean)
+    extends ContextState
+
+}
