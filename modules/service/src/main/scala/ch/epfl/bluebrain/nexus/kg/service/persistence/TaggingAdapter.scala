8a4dd40afd9647e67749d2f6497c00377efa2ab7
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2017-10-30 16:38:35 +0100

    Update tagging adapter for context events

--- modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/persistence/TaggingAdapter.scala
+++ modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/persistence/TaggingAdapter.scala
@@ -1,6 +1,7 @@
 package ch.epfl.bluebrain.nexus.kg.service.persistence
 
 import akka.persistence.journal.{Tagged, WriteEventAdapter}
+import ch.epfl.bluebrain.nexus.kg.core.contexts.ContextEvent
 import ch.epfl.bluebrain.nexus.kg.core.domains.DomainEvent
 import ch.epfl.bluebrain.nexus.kg.core.instances.InstanceEvent
 import ch.epfl.bluebrain.nexus.kg.core.organizations.OrgEvent
@@ -12,11 +13,12 @@ import ch.epfl.bluebrain.nexus.kg.core.schemas.SchemaEvent
 class TaggingAdapter extends WriteEventAdapter {
 
   override def manifest(event: Any): String = ""
-
+//TODO Contexts
   override def toJournal(event: Any): Any = event match {
     case ev: OrgEvent      => Tagged(ev, Set("organization"))
     case ev: DomainEvent   => Tagged(ev, Set("domain"))
     case ev: SchemaEvent   => Tagged(ev, Set("schema"))
+    case ev: ContextEvent  => Tagged(ev, Set("context"))
     case ev: InstanceEvent => Tagged(ev, Set("instance"))
     case _                 => event
   }
