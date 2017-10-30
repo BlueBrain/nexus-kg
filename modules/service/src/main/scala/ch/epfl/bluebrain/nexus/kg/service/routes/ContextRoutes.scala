fda57435e5ec2d69acd7b3b4368d86f3e544bc31
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2017-10-30 16:22:56 +0100

    Add routes for context.

---
+++ modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/routes/ContextRoutes.scala
@@ -0,0 +1,137 @@
+package ch.epfl.bluebrain.nexus.kg.service.routes
+import akka.http.scaladsl.model.headers.OAuth2BearerToken
+import akka.http.scaladsl.model.{StatusCodes, Uri}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import ch.epfl.bluebrain.nexus.commons.iam.IamClient
+import ch.epfl.bluebrain.nexus.commons.iam.acls.Permission._
+import ch.epfl.bluebrain.nexus.kg.core.Fault.CommandRejected
+import ch.epfl.bluebrain.nexus.kg.core.contexts.{Context, ContextId, ContextRef, Contexts}
+import ch.epfl.bluebrain.nexus.kg.core.schemas.SchemaRejection.CannotUnpublishSchema
+import ch.epfl.bluebrain.nexus.kg.service.directives.AuthDirectives._
+import ch.epfl.bluebrain.nexus.kg.service.directives.ResourceDirectives._
+import ch.epfl.bluebrain.nexus.kg.service.io.RoutesEncoder
+import ch.epfl.bluebrain.nexus.kg.service.routes.ContextRoutes._
+import ch.epfl.bluebrain.nexus.kg.service.routes.SchemaRoutes.Publish
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
+import io.circe.{Encoder, Json}
+import kamon.akka.http.KamonTraceDirectives.traceName
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * Http route definitions for context specific functionality
+  * @param contexts  the context operation bundle
+  * @param base      the service public uri + prefix
+  * @param iamClient IAM client
+  * @param ec        execution context
+  */
+class ContextRoutes(contexts: Contexts[Future], base: Uri)(implicit iamClient: IamClient[Future], ec: ExecutionContext)
+    extends DefaultRouteHandling {
+
+  private val contextEncoders = new ContextCustomEncoders(base)
+  import contextEncoders._
+  private val exceptionHandler = ExceptionHandling.exceptionHandler
+
+  override protected def writeRoutes(implicit credentials: Option[OAuth2BearerToken]) =
+    extractContextId { contextId =>
+      pathEndOrSingleSlash {
+        (put & entity(as[Json]) & authorizeResource(contextId, Write)) { json =>
+          parameter('rev.as[Long].?) {
+            case Some(rev) =>
+              traceName("updateContext") {
+                onSuccess(contexts.update(contextId, rev, json)) { ref =>
+                  complete(StatusCodes.OK -> ref)
+                }
+              }
+            case None =>
+              traceName("createContext") {
+                onSuccess(contexts.create(contextId, json)) { ref =>
+                  complete(StatusCodes.Created -> ref)
+
+                }
+              }
+          }
+
+        } ~
+          (delete & authorizeResource(contextId, Write)) {
+            parameter('rev.as[Long]) { rev =>
+              traceName("deprecateContext") {
+                onSuccess(contexts.deprecate(contextId, rev)) { ref =>
+                  complete(StatusCodes.OK -> ref)
+                }
+              }
+            }
+          }
+      } ~
+        (path("config") & authorizeResource(contextId, Publish)) {
+          (pathEndOrSingleSlash & patch & entity(as[ContextConfig])) { cfg =>
+            parameter('rev.as[Long]) { rev =>
+              if (cfg.published) {
+                traceName("publishSchema") {
+                  onSuccess(contexts.publish(contextId, rev)) { ref =>
+                    complete(StatusCodes.OK -> ref)
+                  }
+                }
+              } else exceptionHandler(CommandRejected(CannotUnpublishSchema))
+            }
+          }
+        }
+    }
+
+  override protected def readRoutes(implicit credentials: Option[OAuth2BearerToken]) = {
+    extractContextId { contextId =>
+      (pathEndOrSingleSlash & get & authorizeResource(contextId, Read)) {
+        traceName("getContext") {
+          onSuccess(contexts.fetch(contextId)) {
+            case Some(context) => complete(context)
+            case None          => complete(StatusCodes.NotFound)
+          }
+        }
+      }
+
+    }
+  }
+
+  override protected def searchRoutes(implicit credentials: Option[OAuth2BearerToken]) = reject()
+
+  def routes: Route = combinedRoutesFor("contexts")
+}
+
+object ContextRoutes {
+
+  /**
+    * Contstructs ne ''ContextRoutes'' instance that defines the the http routes specific to contexts.
+    * @param contexts  the context operation bundle
+    * @param base      the service public uri + prefix
+    * @param iamClient IAM client
+    * @param ec        execution context
+    * @return a new ''ContextRoutes'' instance
+    */
+  final def apply(contexts: Contexts[Future], base: Uri)(implicit iamClient: IamClient[Future],
+                                                         ec: ExecutionContext): ContextRoutes =
+    new ContextRoutes(contexts, base)
+
+  /**
+    * Local context config definition that models a resource for context state change operations
+    * @param published whether the context should be published or un-published
+    */
+  final case class ContextConfig(published: Boolean)
+
+}
+
+class ContextCustomEncoders(base: Uri) extends RoutesEncoder[ContextId, ContextRef](base) {
+
+  implicit def contextEncoder: Encoder[Context] = Encoder.encodeJson.contramap { context =>
+    val meta = refEncoder
+      .apply(ContextRef(context.id, context.rev))
+      .deepMerge(
+        Json.obj(
+          "deprecated" -> Json.fromBoolean(context.deprecated),
+          "published"  -> Json.fromBoolean(context.published)
+        )
+      )
+    context.value.deepMerge(meta)
+  }
+}
---
+++ modules/service/src/test/scala/ch/epfl/bluebrain/nexus/kg/service/routes/ContextRoutesSpec.scala
@@ -0,0 +1,210 @@
+package ch.epfl.bluebrain.nexus.kg.service.routes
+
+import java.util.regex.Pattern.quote
+
+import akka.http.scaladsl.model.{StatusCodes, Uri}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.stream.ActorMaterializer
+import cats.instances.future._
+import cats.syntax.show._
+import ch.epfl.bluebrain.nexus.commons.iam.IamClient
+import ch.epfl.bluebrain.nexus.commons.test.{Randomness, Resources}
+import ch.epfl.bluebrain.nexus.commons.types.HttpRejection.IllegalVersionFormat
+import ch.epfl.bluebrain.nexus.kg.core.contexts.ContextRejection._
+import ch.epfl.bluebrain.nexus.kg.core.contexts.{Context, ContextId, ContextRef, Contexts}
+import ch.epfl.bluebrain.nexus.kg.core.domains.DomainRejection.DomainIsDeprecated
+import ch.epfl.bluebrain.nexus.kg.core.domains.{DomainId, Domains}
+import ch.epfl.bluebrain.nexus.kg.core.organizations.{OrgId, Organizations}
+import ch.epfl.bluebrain.nexus.kg.core.schemas.SchemaId
+import ch.epfl.bluebrain.nexus.kg.service.BootstrapService.iamClient
+import ch.epfl.bluebrain.nexus.kg.service.routes.ContextRoutes.ContextConfig
+import ch.epfl.bluebrain.nexus.kg.service.routes.ContextRoutesSpec._
+import ch.epfl.bluebrain.nexus.kg.service.routes.Error.classNameOf
+import ch.epfl.bluebrain.nexus.sourcing.mem.MemoryAggregate
+import ch.epfl.bluebrain.nexus.sourcing.mem.MemoryAggregate._
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
+import io.circe.Json
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpecLike}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+
+class ContextRoutesSpec
+    extends WordSpecLike
+    with Matchers
+    with ScalatestRouteTest
+    with Randomness
+    with Resources
+    with ScalaFutures
+    with MockedIAMClient {
+
+  private implicit val mt: ActorMaterializer        = ActorMaterializer()(system)
+  private implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(3 seconds, 100 millis)
+
+  "ContextRoutes" should {
+
+    val contextJson = jsonContentOf("/contexts/shacl.json")
+
+    val orgAgg = MemoryAggregate("orgs")(Organizations.initial, Organizations.next, Organizations.eval).toF[Future]
+    val orgs   = Organizations(orgAgg)
+    val domAgg =
+      MemoryAggregate("dom")(Domains.initial, Domains.next, Domains.eval)
+        .toF[Future]
+    val doms = Domains(domAgg, orgs)
+    val ctxAgg =
+      MemoryAggregate("contexts")(Contexts.initial, Contexts.next, Contexts.eval)
+        .toF[Future]
+    val contexts = Contexts(ctxAgg, doms, baseUri.toString())
+
+    val orgRef =
+      Await.result(orgs.create(OrgId(genString(length = 3)), Json.obj()), 2 seconds)
+
+    val domRef =
+      Await.result(doms.create(DomainId(orgRef.id, genString(length = 5)), genString(length = 8)), 2 seconds)
+
+    implicit val cl: IamClient[Future] = iamClient("http://localhost:8080")
+
+    val route = ContextRoutes(contexts, baseUri).routes
+
+    val contextId = ContextId(domRef.id, genString(length = 8), genVersion())
+
+    "create a context" in {
+      Put(s"/contexts/${contextId.show}", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.Created
+        responseAs[Json] shouldEqual contextRefAsJson(ContextRef(contextId, 1L))
+      }
+    }
+
+    "reject the creation of a context that already exists" in {
+      Put(s"/contexts/${contextId.show}", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.Conflict
+        responseAs[Error].code shouldEqual classNameOf[ContextAlreadyExists.type]
+      }
+    }
+
+    "reject the creation of a context with illegal version format" in {
+      val id = contextId.show.replace(contextId.version.show, "v1.0")
+      Put(s"/contexts/$id", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[IllegalVersionFormat.type]
+      }
+    }
+
+    "reject the creation of a context with illegal name format" in {
+      Put(s"/contexts/${contextId.copy(name = "@!").show}", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[InvalidContextId.type]
+      }
+    }
+
+    "return the current context" in {
+      Get(s"/contexts/${contextId.show}") ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[Json] shouldEqual Json
+          .obj(
+            "@id"        -> Json.fromString(s"$baseUri/contexts/${contextId.show}"),
+            "rev"        -> Json.fromLong(1L),
+            "deprecated" -> Json.fromBoolean(false),
+            "published"  -> Json.fromBoolean(false)
+          )
+          .deepMerge(contextJson)
+      }
+    }
+
+    "update a context" in {
+      Put(s"/contexts/${contextId.show}?rev=1", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[Json] shouldEqual contextRefAsJson(ContextRef(contextId, 2L))
+      }
+    }
+
+    "reject updating a context which doesn't exits" in {
+      Put(s"/contexts/${contextId.copy(name = "another").show}?rev=1", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.NotFound
+        responseAs[Error].code shouldEqual classNameOf[ContextDoesNotExist.type]
+      }
+    }
+
+    "reject updating a context with incorrect rev" in {
+      Put(s"/contexts/${contextId.show}?rev=10", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.Conflict
+        responseAs[Error].code shouldEqual classNameOf[IncorrectRevisionProvided.type]
+      }
+    }
+
+    "publish a context" in {
+      Patch(s"/contexts/${contextId.show}/config?rev=2", ContextConfig(published = true)) ~> addCredentials(
+        ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[Json] shouldEqual contextRefAsJson(ContextRef(contextId, 3L))
+      }
+      contexts.fetch(contextId).futureValue shouldEqual Some(
+        Context(contextId, 3L, contextJson, deprecated = false, published = true))
+    }
+
+    "reject updating a context when it is published" in {
+      Put(s"/contexts/${contextId.show}?rev=3", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[CannotUpdatePublished.type]
+      }
+    }
+
+    "deprecate a context" in {
+      Delete(s"/contexts/${contextId.show}?rev=3") ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[Json] shouldEqual contextRefAsJson(ContextRef(contextId, 4L))
+      }
+      contexts.fetch(contextId).futureValue shouldEqual Some(
+        Context(contextId, 4L, contextJson, deprecated = true, published = true))
+    }
+
+    "reject updating a context when it is deprecated" in {
+      Put(s"/contexts/${contextId.show}?rev=4", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[ContextIsDeprecated.type]
+      }
+    }
+
+    "reject the creation of schema importing non-existing context" in {
+      val invalidContextId = ContextId(domRef.id, genString(length = 8), genVersion())
+      val invalidContextJson = jsonContentOf(
+        "/contexts/importing-context.json",
+        Map(
+          quote("{{org}}")   -> orgRef.id.id,
+          quote("{{dom}}")   -> domRef.id.id,
+          quote("{{mixed}}") -> genString(length = 8),
+          quote("{{ver}}")   -> genVersion().show
+        )
+      )
+      Put(s"/contexts/${invalidContextId.show}", invalidContextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[IllegalImportsViolation.type]
+      }
+    }
+
+    "reject the creation of context from a deprecated domain" in {
+      //Deprecate the domain
+      doms.deprecate(domRef.id, domRef.rev).futureValue
+      //Create a SchemaId from the deprecated domain
+      val contextId2 = SchemaId(domRef.id, genString(length = 8), genVersion())
+
+      Put(s"/contexts/${contextId2.show}", contextJson) ~> addCredentials(ValidCredentials) ~> route ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Error].code shouldEqual classNameOf[DomainIsDeprecated.type]
+      }
+    }
+
+  }
+}
+
+object ContextRoutesSpec {
+
+  private val baseUri = Uri("http://localhost/v0")
+
+  private def contextRefAsJson(ref: ContextRef) =
+    Json.obj("@id" -> Json.fromString(s"$baseUri/contexts/${ref.id.show}"), "rev" -> Json.fromLong(ref.rev))
+}
