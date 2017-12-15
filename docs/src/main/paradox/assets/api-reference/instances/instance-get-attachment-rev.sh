14c6b6c20496e61c713463843f985fbb9c43ca11
Didac Montero <didac.monteromendez@epfl.ch>
2017-12-15 15:11:20 +0100

    Fixed docs related to instance attachment

--- docs/src/main/paradox/assets/api-reference/instances/instance-get-attachment-rev.sh
+++ docs/src/main/paradox/assets/api-reference/instances/instance-get-attachment-rev.sh
@@ -1,1 +1,1 @@
-curl "https://nexus.example.com/v0/data/myorg/mydom/myschema/1.0.0/42ae55e3-7746-45ed-a9fd-a7ccdd1d736f/attachment?rev=3" -o /path/where/to/store/myfile.png
+curl "https://nexus.example.com/v0/data/myorg/mydom/myschema/1.0.0/42ae55e3-7746-45ed-a9fd-a7ccdd1d736f/attachment?rev=3"
