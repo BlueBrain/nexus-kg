b8822b9d681bb78bbb445e4eab03ce863281060e
Didac Montero <didac.monteromendez@epfl.ch>
2018-01-05 10:40:58 +0100

    Fix changes from review

--- modules/indexing/src/main/scala/ch/epfl/bluebrain/nexus/kg/indexing/query/Sort.scala
+++ modules/indexing/src/main/scala/ch/epfl/bluebrain/nexus/kg/indexing/query/Sort.scala
@@ -17,24 +17,26 @@ final case class SortList(values: List[Sort]) extends Product with Serializable 
   /**
     * @return the string representation of the variables used for sorting (inside the SELECT clause) in SPARQL language
     */
-  def toVars = toVarsMapping map {case (_, variable) => variable} mkString (" ")
+  def toVars = toVarsMapping.map { case (_, variable) => variable }.mkString(" ")
 
   /**
     * @return the string representation of the optional triples used for sorting (inside the WHERE clause) in SPARQL language
     */
   def toTriples =
-    toVarsMapping map {
-      case (Sort(_, predicate), variable) => s"?s <${predicate}> $variable"
-    } mkString ("}\nOPTIONAL{") prefixSuffixNonEmpty ("OPTIONAL{", "}")
+    toVarsMapping
+      .map {
+        case (Sort(_, predicate), variable) => s"?s <${predicate}> $variable"
+      }
+      .mkString("}\nOPTIONAL{") prefixSuffixNonEmpty ("OPTIONAL{", "}")
 
   /**
     * @return the string representation of the ORDER BY clause used for sorting in SPARQL language
     */
   def toOrderByClause =
-    Option(toVarsMapping map {
+    Option(toVarsMapping.map {
       case (Sort(Desc, _), variable) => s"DESC($variable)"
       case (_, variable)             => s"ASC($variable)"
-    }).filterNot(_.isEmpty).getOrElse(List("?s")) mkString (" ")
+    }).filterNot(_.isEmpty).getOrElse(List("?s")).mkString(" ")
 }
 
 object SortList {
