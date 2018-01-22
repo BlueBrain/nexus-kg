8d8fad4c933988b32db2910181533e3d7fffdba9
Didac Montero <didac.monteromendez@epfl.ch>
2018-01-22 15:47:31 +0100

    Added Stringbuilder to improve performance

--- modules/elastic/src/main/scala/ch/epfl/bluebrain/nexus/kg/indexing/PatchQuery.scala
+++ modules/elastic/src/main/scala/ch/epfl/bluebrain/nexus/kg/indexing/PatchQuery.scala
@@ -15,7 +15,8 @@ object PatchQuery {
     * @param removeKeys the keys to be removed from the existing document
     */
   def apply(json: Json, removeKeys: String*): Json = {
-    val script = removeKeys.foldLeft("")((acc, field) => acc ++ s"""ctx._source.remove("$field");""")
+    val script = removeKeys
+      .foldLeft(StringBuilder.newBuilder)((acc, field) => acc.append(s"""ctx._source.remove("$field");"""))
     Json.obj(
       "script" -> Json.obj("source" -> Json.fromString(s"${script}ctx._source.putAll(params.value)"),
                            "params" -> Json.obj("value" -> json)))
@@ -30,10 +31,10 @@ object PatchQuery {
     * @param keepKeys the keys to be kept from the existing document
     */
   def inverse(json: Json, keepKeys: String*): Json = {
-    val (prefix, sufix) = keepKeys.zipWithIndex.foldLeft("" -> "") {
+    val (prefix, sufix) = keepKeys.zipWithIndex.foldLeft(StringBuilder.newBuilder -> StringBuilder.newBuilder) {
       case ((p, s), (key, index)) =>
         val variable = s"var$index"
-        (p + s"""Object $variable = ctx._source.get("$key");""") -> (s + s"""if($variable != null) ctx._source.put("$key", $variable);""")
+        (p.append(s"""Object $variable = ctx._source.get("$key");""")) -> (s.append(s"""if($variable != null) ctx._source.put("$key", $variable);"""))
     }
     val script = s"""$prefix ctx._source = params.value; $sufix"""
     Json.obj("script" -> Json.obj("source" -> Json.fromString(script), "params" -> Json.obj("value" -> json)))
