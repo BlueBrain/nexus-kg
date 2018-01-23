e7c11455381a3ce6bfe7f514a82ca9f9711d5457
Henry Genet <henry.genet@epfl.ch>
2018-01-23 17:57:40 +0100

    Fix cyclic reference in resource/standards context

--- modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/routes/ContextRoutes.scala
+++ modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/routes/ContextRoutes.scala
@@ -225,6 +225,8 @@ object ContextRoutes {
 class ContextCustomEncoders(base: Uri, prefixes: PrefixUris)(implicit E: Context => ContextId)
     extends RoutesEncoder[ContextId, ContextRef, Context](base, prefixes) {
 
+  private val excluded = List(prefixes.CoreContext.context, prefixes.StandardsContext.context)
+
   implicit val contextRefEncoder: Encoder[ContextRef] = refEncoder.mapJson(_.addCoreContext)
 
   implicit val contextEncoder: Encoder[Context] = Encoder.encodeJson.contramap { ctx =>
@@ -237,7 +239,7 @@ class ContextCustomEncoders(base: Uri, prefixes: PrefixUris)(implicit E: Context
           JsonLDKeys.nxvPublished  -> Json.fromBoolean(ctx.published)
         )
       )
-    if (ctx.id.qualify == prefixes.CoreContext.context)
+    if (excluded.contains(ctx.id.qualify))
       ctx.value.deepMerge(meta)
     else
       ctx.value.deepMerge(meta).addCoreContext
