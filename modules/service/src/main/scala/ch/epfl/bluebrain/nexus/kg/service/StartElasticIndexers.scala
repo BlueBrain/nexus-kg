79c532e82f15dc020f5174ee7225525843be5a7d
Didac <didac.montero.mendez@gmail.com>
2018-01-23 17:50:20 +0100

    Trigger the start of ElasticSearch indexing (#206)

--- modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/StartElasticIndexers.scala
+++ modules/service/src/main/scala/ch/epfl/bluebrain/nexus/kg/service/StartElasticIndexers.scala
@@ -8,10 +8,17 @@ import akka.http.scaladsl.model.Uri
 import cats.instances.future._
 import ch.epfl.bluebrain.nexus.commons.es.client.ElasticClient
 import ch.epfl.bluebrain.nexus.commons.service.persistence.SequentialTagIndexer
-import ch.epfl.bluebrain.nexus.kg.core.contexts.Contexts
+import ch.epfl.bluebrain.nexus.kg.core.contexts.{ContextEvent, Contexts}
+import ch.epfl.bluebrain.nexus.kg.core.domains.DomainEvent
+import ch.epfl.bluebrain.nexus.kg.core.instances.InstanceEvent
 import ch.epfl.bluebrain.nexus.kg.core.organizations.OrgEvent
+import ch.epfl.bluebrain.nexus.kg.core.schemas.SchemaEvent
 import ch.epfl.bluebrain.nexus.kg.indexing.ElasticIndexingSettings
+import ch.epfl.bluebrain.nexus.kg.indexing.contexts.ContextElasticIndexer
+import ch.epfl.bluebrain.nexus.kg.indexing.domains.DomainElasticIndexer
+import ch.epfl.bluebrain.nexus.kg.indexing.instances.InstanceElasticIndexer
 import ch.epfl.bluebrain.nexus.kg.indexing.organizations._
+import ch.epfl.bluebrain.nexus.kg.indexing.schemas.SchemaElasticIndexer
 import ch.epfl.bluebrain.nexus.kg.service.config.Settings
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -37,22 +44,61 @@ class StartElasticIndexers(settings: Settings,
   private implicit val config: Configuration =
     Configuration.default.withDiscriminator("type")
 
+  private val indexingSettings = ElasticIndexingSettings(settings.Elastic.IndexPrefix,
+                                                         settings.Elastic.Type,
+                                                         apiUri,
+                                                         settings.Prefixes.CoreVocabulary)
+
   startIndexingOrgs()
+  startIndexingDoms()
+  startIndexingContexts()
+  startIndexingSchemas()
+  startIndexingInstances()
 
-  private def startIndexingOrgs() = {
-    val orgIndexingSettings = ElasticIndexingSettings(settings.Elastic.IndexPrefix,
-                                                      settings.Elastic.Type,
-                                                      apiUri,
-                                                      settings.Prefixes.CoreVocabulary)
-
+  private def startIndexingOrgs() =
     SequentialTagIndexer.start[OrgEvent](
-      OrganizationElasticIndexer[Future](elasticClient, contexts, orgIndexingSettings).apply _,
-      "organization-es-to-3s",
+      OrganizationElasticIndexer[Future](elasticClient, contexts, indexingSettings).apply _,
+      "organization-to-elastic",
       settings.Persistence.QueryJournalPlugin,
       "organization",
-      "sequential-organization-es-indexer"
+      "sequential-organization-elastic-indexer"
     )
-  }
+
+  private def startIndexingDoms() =
+    SequentialTagIndexer.start[DomainEvent](
+      DomainElasticIndexer[Future](elasticClient, indexingSettings).apply _,
+      "domain-to-elastic",
+      settings.Persistence.QueryJournalPlugin,
+      "domain",
+      "sequential-domain-elastic-indexer"
+    )
+
+  private def startIndexingContexts() =
+    SequentialTagIndexer.start[ContextEvent](
+      ContextElasticIndexer[Future](elasticClient, indexingSettings).apply _,
+      "context-to-elastic",
+      settings.Persistence.QueryJournalPlugin,
+      "context",
+      "sequential-context-elastic-indexer"
+    )
+
+  private def startIndexingSchemas() =
+    SequentialTagIndexer.start[SchemaEvent](
+      SchemaElasticIndexer[Future](elasticClient, contexts, indexingSettings).apply _,
+      "schema-to-elastic",
+      settings.Persistence.QueryJournalPlugin,
+      "schema",
+      "sequential-schema-elastic-indexer"
+    )
+
+  private def startIndexingInstances() =
+    SequentialTagIndexer.start[InstanceEvent](
+      InstanceElasticIndexer[Future](elasticClient, contexts, indexingSettings).apply _,
+      "instance-to-elastic",
+      settings.Persistence.QueryJournalPlugin,
+      "instance",
+      "sequential-instance-elastic-indexer"
+    )
 }
 
 object StartElasticIndexers {
