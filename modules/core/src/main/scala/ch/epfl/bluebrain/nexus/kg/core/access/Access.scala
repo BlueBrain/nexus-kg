7ea4ff554522f5051957b904d8b5171f42119350
Henry <hygt@users.noreply.github.com>
2018-05-04 10:24:30 +0200

    Add project authorization directive (#313)

--- modules/core/src/main/scala/ch/epfl/bluebrain/nexus/kg/core/access/Access.scala
+++ modules/core/src/main/scala/ch/epfl/bluebrain/nexus/kg/core/access/Access.scala
@@ -2,39 +2,42 @@ package ch.epfl.bluebrain.nexus.kg.core.access
 
 /**
   * Enumeration type for access grants used to restrict operations on resources.
+  *
+  * @param name the access grant string value
   */
-sealed trait Access extends Product with Serializable
+sealed abstract class Access(val name: String) extends Product with Serializable
 
 object Access {
 
   /**
     * Read access grant
     */
-  final case object Read extends Access
+  final case object Read extends Access("read")
 
   /**
     * Write access grant
     */
-  final case object Write extends Access
+  final case object Write extends Access("write")
 
   /**
     * Create access grant
     */
-  final case object Create extends Access
+  final case object Create extends Access("create")
 
   /**
     * Attach access grant
     */
-  final case object Attach extends Access
+  final case object Attach extends Access("attach")
 
   /**
     * Manage access grant
     */
-  final case object Manage extends Access
+  final case object Manage extends Access("manage")
 
   type Read   = Read.type
   type Write  = Write.type
   type Create = Create.type
   type Attach = Attach.type
   type Manage = Manage.type
+
 }
--- modules/core/src/main/scala/ch/epfl/bluebrain/nexus/kg/core/resources/ResourceType.scala
+++ modules/core/src/main/scala/ch/epfl/bluebrain/nexus/kg/core/resources/ResourceType.scala
@@ -2,27 +2,30 @@ package ch.epfl.bluebrain.nexus.kg.core.resources
 
 /**
   * Enumeration type for all resource types that the platform recognized.
+  *
+  * @param name the resource type string value
   */
-sealed trait ResourceType extends Product with Serializable
+sealed abstract class ResourceType(val name: String) extends Product with Serializable
 
 object ResourceType {
 
   /**
     * Schema resource type
     */
-  final case object Schema extends ResourceType
+  final case object Schema extends ResourceType("schema")
 
   /**
     * Instance resource type
     */
-  final case object Instance extends ResourceType
+  final case object Instance extends ResourceType("instance")
 
   /**
     * Ontology resource type
     */
-  final case object Ontology extends ResourceType
+  final case object Ontology extends ResourceType("ontology")
 
   type SchemaType   = Schema.type
   type InstanceType = Instance.type
   type OntologyType = Ontology.type
+
 }
