b37b390992c0ddf93a8e5d5b6206484923ee7116
Henry Genet <henry.genet@epfl.ch>
2018-06-12 11:33:24 +0200

    Add in-project resolution

---
+++ src/main/scala/ch/epfl/bluebrain/nexus/kg/resolve/InProjectResolution.scala
@@ -0,0 +1,26 @@
+package ch.epfl.bluebrain.nexus.kg.resolve
+
+import cats.Functor
+import cats.syntax.functor._
+import ch.epfl.bluebrain.nexus.kg.resources.Ref._
+import ch.epfl.bluebrain.nexus.kg.resources._
+
+/**
+  * Simplest implementation that handles the resolution process of references to resources
+  * within a given project.
+  *
+  * @param project the resolution scope
+  * @param repo    the implicitly available resource repository
+  * @tparam F      the resolution effect type
+  */
+class InProjectResolution[F[_]: Functor](project: ProjectRef)(implicit repo: Repo[F]) extends Resolution[F] {
+
+  override def resolve(ref: Ref): F[Option[Resource]] = ref match {
+    case Latest(value)        => Resources.get(Id[ProjectRef](project, value)).value
+    case Revision(value, rev) => Resources.get(Id[ProjectRef](project, value), rev).value
+    case Tag(value, tag)      => Resources.get(Id[ProjectRef](project, value), tag).value
+  }
+
+  override def resolveAll(ref: Ref): F[List[Resource]] =
+    resolve(ref).map(_.toList)
+}
