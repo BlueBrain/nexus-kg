fb5096bc7c3c0b051a7ff575c89cb242da398080
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2018-07-11 17:10:07 +0200

    Use .addContext method

--- src/main/scala/ch/epfl/bluebrain/nexus/kg/search/QueryResultEncoder.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/kg/search/QueryResultEncoder.scala
@@ -4,6 +4,7 @@ import cats.syntax.show._
 import ch.epfl.bluebrain.nexus.commons.types.search.QueryResults.{ScoredQueryResults, UnscoredQueryResults}
 import ch.epfl.bluebrain.nexus.kg.config.Contexts
 import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
+import ch.epfl.bluebrain.nexus.rdf.syntax.circe.context._
 import io.circe.{Encoder, Json}
 
 object QueryResultEncoder {
@@ -12,37 +13,39 @@ object QueryResultEncoder {
     * Encoder for scored query results
     */
   implicit val scoredEncoder: Encoder[ScoredQueryResults[AbsoluteIri]] = Encoder.instance { results =>
-    Json.obj(
-      "@context" -> Json.fromString(Contexts.searchCtxUri.show),
-      "total"    -> Json.fromLong(results.total),
-      "maxScore" -> Json.fromFloatOrString(results.maxScore),
-      "results" -> Json.arr(
-        results.results.map { res =>
-          Json.obj(
-            "resultId" -> Json.fromString(res.source.show)
-          )
+    Json
+      .obj(
+        "total"    -> Json.fromLong(results.total),
+        "maxScore" -> Json.fromFloatOrString(results.maxScore),
+        "results" -> Json.arr(
+          results.results.map { res =>
+            Json.obj(
+              "resultId" -> Json.fromString(res.source.show)
+            )
 
-        }: _*
+          }: _*
+        )
       )
-    )
+      .addContext(Contexts.searchCtxUri)
   }
 
   /**
     * Encoder for unscored query results
     */
   implicit val unscoredEncoder: Encoder[UnscoredQueryResults[AbsoluteIri]] = Encoder.instance { results =>
-    Json.obj(
-      "@context" -> Json.fromString(Contexts.searchCtxUri.show),
-      "total"    -> Json.fromLong(results.total),
-      "results" -> Json.arr(
-        results.results.map { res =>
-          Json.obj(
-            "resultId" -> Json.fromString(res.source.show)
-          )
+    Json
+      .obj(
+        "total" -> Json.fromLong(results.total),
+        "results" -> Json.arr(
+          results.results.map { res =>
+            Json.obj(
+              "resultId" -> Json.fromString(res.source.show)
+            )
 
-        }: _*
+          }: _*
+        )
       )
-    )
+      .addContext(Contexts.searchCtxUri)
   }
 
 }
