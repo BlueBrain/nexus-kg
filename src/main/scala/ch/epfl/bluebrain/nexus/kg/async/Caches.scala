bc932be196f0098518e1b4d7dc1092c87d62daf2
Didac Montero <didac.monteromendez@epfl.ch>
2019-01-16 11:31:52 +0100

    Fixed typo

--- src/main/scala/ch/epfl/bluebrain/nexus/kg/async/Caches.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/kg/async/Caches.scala
@@ -6,6 +6,6 @@ package ch.epfl.bluebrain.nexus.kg.async
   * @param project      the project cache
   * @param view         the view cache
   * @param resolver     the resolver cache
-  * @tparam F the effet type
+  * @tparam F the effect type
   */
 final case class Caches[F[_]](project: ProjectCache[F], view: ViewCache[F], resolver: ResolverCache[F])
