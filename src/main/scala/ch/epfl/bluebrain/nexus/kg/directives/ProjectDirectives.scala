be7ba1dd52558b8fe984c2acd333ba11317de6fb
Didac <didac.monteromendez@epfl.ch>
2019-05-15 16:38:13 +0200

    Supported API interaction using organization and project UUIDs (#663)
    * Supported API interaction using organization and project UUIDs
    
    * Removed dup.

--- src/main/scala/ch/epfl/bluebrain/nexus/kg/directives/ProjectDirectives.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/kg/directives/ProjectDirectives.scala
@@ -1,5 +1,7 @@
 package ch.epfl.bluebrain.nexus.kg.directives
 
+import java.util.UUID
+
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directive0, Directive1}
 import ch.epfl.bluebrain.nexus.admin.client.AdminClient
@@ -9,11 +11,13 @@ import ch.epfl.bluebrain.nexus.kg.KgError.{OrganizationNotFound, ProjectIsDeprec
 import ch.epfl.bluebrain.nexus.kg.cache.ProjectCache
 import ch.epfl.bluebrain.nexus.kg.config.Schemas
 import ch.epfl.bluebrain.nexus.kg.config.Vocabulary.nxv
-import ch.epfl.bluebrain.nexus.kg.resources.ProjectLabel
+import ch.epfl.bluebrain.nexus.kg.resources.{OrganizationRef, ProjectLabel, ProjectRef}
 import ch.epfl.bluebrain.nexus.kg.resources.syntax._
 import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
 import monix.eval.Task
 import monix.execution.Scheduler
+
+import scala.util.{Success, Try}
 
 object ProjectDirectives {
   private val defaultPrefixMapping: Map[String, AbsoluteIri] = Map(
@@ -36,25 +40,51 @@ object ProjectDirectives {
   def project(implicit projectCache: ProjectCache[Task],
               client: AdminClient[Task],
               cred: Option[AuthToken],
-              s: Scheduler): Directive1[Project] =
+              s: Scheduler): Directive1[Project] = {
+
+    def projectByLabel(orgLabel: String, projectLabel: String): Directive1[Project] = {
+      val label = ProjectLabel(orgLabel, projectLabel)
+      val result = projectCache
+        .getBy(label)
+        .flatMap {
+          case value @ Some(_) => Task.pure(value)
+          case None            => client.fetchProject(orgLabel, projectLabel)
+        }
+        .onErrorRecoverWith {
+          case _ => client.fetchProject(orgLabel, projectLabel)
+        }
+      onSuccess(result.runToFuture)
+        .flatMap {
+          case None          => failWith(ProjectNotFound(label))
+          case Some(project) => provide(addDefaultMappings(project))
+        }
+    }
+
+    def projectByUuid(orgUuid: UUID, projUuid: UUID): Directive1[Project] = {
+      val result = projectCache
+        .get(OrganizationRef(orgUuid), ProjectRef(projUuid))
+        .flatMap {
+          case value @ Some(_) => Task.pure(value)
+          case None            => client.fetchProject(orgUuid, projUuid)
+        }
+        .onErrorRecoverWith {
+          case _ => client.fetchProject(orgUuid, projUuid)
+        }
+      onSuccess(result.runToFuture)
+        .flatMap {
+          case None          => reject
+          case Some(project) => provide(addDefaultMappings(project))
+        }
+    }
+
     pathPrefix(Segment / Segment).tflatMap {
       case (orgLabel, projectLabel) =>
-        val label = ProjectLabel(orgLabel, projectLabel)
-        val result = projectCache
-          .getBy(label)
-          .flatMap {
-            case value @ Some(_) => Task.pure(value)
-            case _               => client.fetchProject(orgLabel, projectLabel)
-          }
-          .onErrorRecoverWith {
-            case _ => client.fetchProject(orgLabel, projectLabel)
-          }
-        onSuccess(result.runToFuture)
-          .flatMap {
-            case None          => failWith(ProjectNotFound(label))
-            case Some(project) => provide(addDefaultMappings(project))
-          }
+        Try((UUID.fromString(orgLabel), UUID.fromString(projectLabel))) match {
+          case Success((orgUuid, projUuid)) => projectByUuid(orgUuid, projUuid) | projectByLabel(orgLabel, projectLabel)
+          case _                            => projectByLabel(orgLabel, projectLabel)
+        }
     }
+  }
 
   /**
     * Fetches organization configuration from nexus admin.
@@ -63,12 +93,25 @@ object ProjectDirectives {
     */
   def org(label: String)(implicit client: AdminClient[Task],
                          cred: Option[AuthToken],
-                         s: Scheduler): Directive1[Organization] =
-    onSuccess(client.fetchOrganization(label).runToFuture)
-      .flatMap {
-        case None          => failWith(OrganizationNotFound(label))
-        case Some(project) => provide(project)
-      }
+                         s: Scheduler): Directive1[Organization] = {
+    def orgByLabel: Directive1[Organization] =
+      onSuccess(client.fetchOrganization(label).runToFuture)
+        .flatMap {
+          case None          => failWith(OrganizationNotFound(label))
+          case Some(project) => provide(project)
+        }
+    def orgByUuid(uuid: UUID): Directive1[Organization] =
+      onSuccess(client.fetchOrganization(uuid).runToFuture)
+        .flatMap {
+          case None          => reject()
+          case Some(project) => provide(project)
+        }
+
+    Try(UUID.fromString(label)) match {
+      case Success(uuid) => orgByUuid(uuid) | orgByLabel
+      case _             => orgByLabel
+    }
+  }
 
   private def addDefaultMappings(project: Project) =
     project.copy(apiMappings = project.apiMappings ++ defaultPrefixMapping)
