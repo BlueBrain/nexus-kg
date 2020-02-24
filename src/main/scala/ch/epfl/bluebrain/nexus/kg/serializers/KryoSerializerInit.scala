ace8d7db44dc75c4a161726811365cb72142a142
Didac <didac.monteromendez@epfl.ch>
2020-02-24 11:41:11 +0100

    Fixed Path kryo deserialization (#769)
    * Fixed Path kryo deserialization
    
    * Fixed formatting

---
+++ src/main/scala/ch/epfl/bluebrain/nexus/kg/serializers/KryoSerializerInit.scala
@@ -0,0 +1,27 @@
+package ch.epfl.bluebrain.nexus.kg.serializers
+
+import java.nio.file.Path
+
+import com.esotericsoftware.kryo.{Kryo, Serializer}
+import com.esotericsoftware.kryo.io.{Input, Output}
+import io.altoo.akka.serialization.kryo.DefaultKryoInitializer
+import io.altoo.akka.serialization.kryo.serializer.scala.ScalaKryo
+
+class PathSerializer extends Serializer[Path] {
+
+  override def write(kryo: Kryo, output: Output, path: Path): Unit =
+    output.writeString(path.toString)
+
+  override def read(kryo: Kryo, input: Input, `type`: Class[Path]): Path =
+    Path.of(input.readString())
+}
+
+class KryoSerializerInit extends DefaultKryoInitializer {
+
+  override def postInit(kryo: ScalaKryo): Unit = {
+    super.postInit(kryo)
+    kryo.addDefaultSerializer(classOf[Path], classOf[PathSerializer])
+    kryo.register(classOf[Path], new PathSerializer)
+    ()
+  }
+}
